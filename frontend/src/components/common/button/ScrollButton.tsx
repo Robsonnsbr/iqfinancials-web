fb339030b74018a679095d6d5dd898ed63ef9571
robsonnsbr <robsonnsbr@gmail.com>
2025-01-16 16:37:28 -0300

    style: change size scrolltop

--- frontend/src/components/common/button/ScrollButton.tsx
+++ frontend/src/components/common/button/ScrollButton.tsx
@@ -20,9 +20,9 @@ const ScrollButton = () => {
       title="Voltar ao Topo"
       aria-label="Voltar ao Topo"
       onClick={scrollToTop}
-      className="fixed z-50 bottom-28 right-8  bg-blue text-white w-[60px] h-[60px] rounded-full shadow-md hover:bg-blue-light hover:scale-110 active:scale-100 active:text-red"
+      className="fixed z-50 bottom-28 right-12  bg-blue text-white w-[30px] h-[30px] rounded-full shadow-md hover:bg-blue-light hover:scale-110 active:scale-100 active:text-red"
     >
-      <FiArrowUp className="size-10 m-auto" />
+      <FiArrowUp className="size-5 m-auto" />
     </button>
   );
 };
