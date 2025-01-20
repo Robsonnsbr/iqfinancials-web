858aebb34a6a4f41428cd111af9f0e2e2cb0072a
robsonnsbr <robsonnsbr@gmail.com>
2025-01-20 17:30:58 -0300

    fix: add label icon 404

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -11,7 +11,10 @@ export default function NotFound() {
       <SectionContainer id="not-found-section ">
         <div className="text-center text-white bg-red p-10">
           <h1 aria-label="Erro 404">
-            <TbError404Off aria-hidden="true" className="size-32 m-auto" />
+            <TbError404Off
+              aria-label="icon erro 404"
+              className="size-32 m-auto"
+            />
           </h1>
           <p className="text-2xl mb-6">Página não encontrada</p>
           <Link
