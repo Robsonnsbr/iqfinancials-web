f38875132fa07f6c8d4b2ceeb67dced7ce089858
robsonnsbr <robsonnsbr@gmail.com>
2025-01-27 19:09:17 -0300

    test: ..

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -1,27 +1,27 @@
-import Main from "@components/common/main";
-import ContentContainer from "@components/common/containers/ContentContainer";
-import Footer from "@components/common/footer";
+// import Main from "@components/common/main";
+// import ContentContainer from "@components/common/containers/ContentContainer";
+// import Footer from "@components/common/footer";
 
-// Função assíncrona para lidar com props
-export default async function DynamicPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } = params; // Aguarde a resolução do `params`
+// // Função assíncrona para lidar com props
+// export default async function DynamicPage({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   const { slug } = params; // Aguarde a resolução do `params`
 
-  return (
-    <>
-      <Main>
-        <section className="global-section" id="section-dynamic">
-          <ContentContainer>
-            <div className="w-full h-full flex flex-col">
-              <h1 className="text-white">Página dinâmica: {slug}</h1>
-            </div>
-          </ContentContainer>
-        </section>
-      </Main>
-      <Footer />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Main>
+//         <section className="global-section" id="section-dynamic">
+//           <ContentContainer>
+//             <div className="w-full h-full flex flex-col">
+//               <h1 className="text-white">Página dinâmica: {slug}</h1>
+//             </div>
+//           </ContentContainer>
+//         </section>
+//       </Main>
+//       <Footer />
+//     </>
+//   );
+// }
